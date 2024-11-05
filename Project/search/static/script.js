88576e1658a9b196f222e3a9deac717580115a70
Chí Sơn <caosychison03@gmail.com>
2024-11-05 15:54:38 +0700

    update css

--- Project/search/static/script.js
+++ Project/search/static/script.js
@@ -26,6 +26,16 @@ document.getElementById('search-box').addEventListener('input', function() {
         searchResults.style.display = 'none';
     }
 });
+
+document.getElementById('search-button').addEventListener('click', function() {
+    const query = document.getElementById('search-box').value;
+    const searchResults = document.getElementById('search-results');
+    if (query.length > 0 && searchResults.childNodes.length > 0) {
+        const firstResult = searchResults.childNodes[0];
+        firstResult.click();
+    }
+});
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('search-results').addEventListener('click', function (event) {
         if (event.target && event.target.classList.contains('stock-item')) {
@@ -34,5 +44,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-
